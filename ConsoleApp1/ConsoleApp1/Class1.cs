11d9d6c9bbf97c3314a763fb7c68ecfc4a9a5bae
FIXED-TERM Huseyn Mirzayev (HtvP/MSE3.3-T) <hum4htv@bosch.com>
2023-05-18 15:48:12 +0200

    hello

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public string? name { get; set; }
+	public string? profession { get; set; }
+
+	public Class1(string name, string profession)
+	{
+		this.name = name;
+		this.profession = profession;
+	}
+}
