d628110e57d31282b1bc6a2a74a827e0f45e356f
FIXED-TERM Huseyn Mirzayev (HtvP/MSE3.3-T) <hum4htv@bosch.com>
2023-05-18 16:57:09 +0200

    first commit1

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-public class Class1
+public class Class1 : AgeClass
 {
 	public string? name { get; set; }
 	public string? profession { get; set; }
 
-	public Class1(string name, string profession)
+	public Class1(string name, string profession, int age)
 	{
+		this.age = age;
 		this.name = name;
 		this.profession = profession;
 	}
---
+++ ConsoleApp1/ConsoleApp1/age.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class AgeClass
+{
+	public int age { get; set; }
+
+
+	public AgeClass(int age)
+	{
+		this.age = age;
+	}
+}
